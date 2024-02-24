8fe0a4ed52b87a6989e66b9bdc7fe8d40d37fecc
RJW <blacksheeprobotics@gmail.com>
2024-02-24 00:32:29 -0500

    Added memory to save to csv of remaining states to learn

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import turtle
 # import csv file:
 state_data = pandas.read_csv("50_states.csv")
 number_of_states = len(state_data["state"])
-print(number_of_states)
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -18,6 +17,15 @@ keep_going = True
 guessed_states = []
 while len(guessed_states) < number_of_states:
     answer = (screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another State's name? ")).title()
+    if answer == "Exit":
+        states_to_learn = []
+        state_list = state_data["state"].values.tolist()
+        for state in state_list:
+            if state not in guessed_states:
+                states_to_learn.append(state)
+        states_to_learn_dataFrame = pandas.DataFrame({"state": states_to_learn})
+        states_to_learn_dataFrame.to_csv("states_to_learn.csv")
+        break
     if answer not in guessed_states:
         guessed_states.append(answer)
         get_coordinates = state_data[state_data["state"] == answer.title()]
